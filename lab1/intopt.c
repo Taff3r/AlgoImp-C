2573794955c988d775d7686dd082ff8b5136d2d4
Simon Tenggren <simon.tenggren@gmail.com>
2020-04-08 17:14:33 +0200

    basic simplex algo implemented

--- lab1/intopt.c
+++ lab1/intopt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "simplex.h"
 #include "intopt.h"
+#include "math.h"
 #define EPSILON (0.0000001)
 int main(){
     // 1
-    simplex_t* simp = malloc(sizeof(simplex_t));
     FILE* fp;
     fp = fopen("constraints", "r");
     // Scan m && n
@@ -33,12 +33,9 @@ int main(){
 
     int* var  = (int*) calloc(n + m, sizeof(int)); // var are zeroes
     double* x = (double*) calloc(n + 1, sizeof(double)); // x are zeroes
-
-    int k  = init(simp, m, n, a, b, x, c, var);
-    int nbi= select_nonbasic(simp); 
-    pivot(simp, 0,1);
-    printf("%d\n", k);
-
+    double y = 0;
+    double res = simplex(m, n, a, b, c, x, y);
+    printf("%lf", res);
     // Free resources
     free(c);
     free(b);
@@ -48,13 +45,12 @@ int main(){
     free(a); // Free other pointer
     free(x);
     fclose(fp);
-    printConstraints(simp);
-    delete_simp(simp);
 }
 
-int init(simplex_t* s, int m, int n, double** a, double* b, double* x, double* c, int* var) {
+int init(simplex_t* s, int m, int n, double** a, double* b, double* c, double* x, double y, int* var) {
     int i, k; 
     init_simplex_t(s, m, n);
+    s->y = y;
     // Assign c
     for (int l = 0; l != n; ++l)
         s->c[l] = c[l];
@@ -73,7 +69,6 @@ int init(simplex_t* s, int m, int n, double** a, double* b, double* x, double* c
         if (b[i] < b[k])
             k = i;
     }
-
     return k;
 }
 
@@ -93,7 +88,6 @@ void pivot(simplex_t* s, int row, int col){
     int      m = s->m;
     int      n = s->n;
     int      i,j,t;
-
     t = s->var[col];
     s->var[col] = s->var[n + row];
     s->var[n+row] = t;
@@ -103,16 +97,17 @@ void pivot(simplex_t* s, int row, int col){
         if (i != col) {
             c[i] = c[i] - c[col] * a[row][i] / a[row][col];
         }
-        c[col] = -c[col] / a[row][col];
-    }
 
-    for(i = 0; i < n; ++i){
+    }
+    // HERE
+    c[col] = -c[col] / a[row][col];
+    for(i = 0; i < m; ++i){
         if(i != row) {
             b[i] = b[i] - a[i][col] * b[row] / a[row][col];
         }
     }
 
-    for (i = 0; i < n; ++i){
+    for (i = 0; i < m; ++i){
         if( i != row) {
             for(j = 0; j < n; ++j){
                 if(j != col) {
@@ -136,4 +131,66 @@ void pivot(simplex_t* s, int row, int col){
 
     b[row] = b[row] / a[row][col];
     a[row][col] = 1 / a[row][col]; 
+}
+
+int initial(simplex_t* s, int m, int n, double** a, double* b, double* c, double* x , double y, int* var) {
+    int i,j,k;
+    double w;
+    k = init(s, m, n, a, b, c, x, y, var);
+    return 1;
+    /*
+       if (b[k] >= 0) {
+
+       }
+       */
+}
+
+double xsimplex(int m, int n, double** a, double* b, double* c, double* x, double y, int* var, int h){
+    simplex_t* s = malloc(sizeof(simplex_t));
+    // No c in initial?
+    if (!initial(s, m, n, a, b, c, x, y, var)) {
+        delete_simp(s);
+        return NAN; 
+    }
+    int row, col;
+    while((col = select_nonbasic(s)) >= 0){
+        row = -1; 
+        for(int i = 0; i < m; ++i){
+            if (a[i][col] > EPSILON && (row < 0 || (b[i] / a[i][col]) < (b[row] / a[row][col]))){
+                row = i;
+            }
+        }
+        if (row < 0 ){
+            free(s->var);
+            return INFINITY;
+        }
+        pivot(s, row, col);
+    }
+
+    if (h == 0) {
+        for (int i = 0; i < n; ++i){
+            if (s->var[i] < n){
+                x[s->var[i]] = 0;
+            }
+        }
+
+        for(int i = 0; i < m; ++i){
+            if (s->var[n + 1] < n) {
+                x[s->var[n + i]] = s->b[i];
+            }
+        }
+    } else {
+        for (int i = 0; i < n; ++i){
+            x[s->var[i]] = 0;
+        }
+        for (int i = n; i < n+m; ++i){
+            x[s->var[i]] = s->b[i - n];
+        }
+    }
+    double ret = s->y;
+    delete_simp(s);
+    return ret;
+}
+double simplex(int m, int n, double** a, double* b, double* c, double* x, double y) {
+    return xsimplex(m, n, a, b, c, x, y, NULL, 0);
 }
--- lab1/intopt.h
+++ lab1/intopt.h
@@ -1,7 +1,10 @@
 #ifndef INTOPT_H
 #define INTOPT_H
 #include "simplex.h"
-int init(simplex_t*, int, int, double**, double*, double*, double*, int*);
+int init(simplex_t*, int m, int n, double** a, double* b, double* c, double * x, double y, int* var);
 int select_nonbasic(simplex_t*);
 void pivot(simplex_t*, int, int);
+double xsimplex(int m, int n, double** a, double* b, double* c, double* x, double y, int* var, int h);
+int initial(simplex_t* s, int m, int n, double** a, double* b, double* c, double* x, double y, int*);
+double simplex(int m, int n, double** a, double* b, double* c, double* x, double y);
 #endif
